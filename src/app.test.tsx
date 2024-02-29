918da9d3324d644f49c4815f06050c3a57d92f74
Cameron Fry <crfry123@gmail.com>
2024-02-29 13:02:05 +1100

    fixed tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -6,11 +6,11 @@ import { Theme } from "@radix-ui/themes";
 import { Provider } from "react-redux";
 import userEvent from "@testing-library/user-event";
 
-const user = userEvent.setup()
-const store = configureStore([])({sum: {value: 0}});
+const user = userEvent.setup();
+const store = configureStore([])({ sum: { value: 0 } });
 
 test("handles actions", async () => {
-   render(
+  render(
     <Provider store={store}>
       <Theme>
         <App />
@@ -18,8 +18,8 @@ test("handles actions", async () => {
     </Provider>
   );
 
-  await user.type(screen.getByPlaceholderText('one'), "1")
-  await user.type(screen.getByPlaceholderText('two'), "1")
-  await user.click(screen.getByText('Go!'))
-  expect(store.getActions()[0].type).toEqual('todos/sum')
+  await user.type(screen.getByPlaceholderText("one"), "1");
+  await user.type(screen.getByPlaceholderText("two"), "1");
+  await user.click(screen.getByText("Calculate"));
+  expect(store.getActions()[0].type).toEqual("todos/sum");
 });
